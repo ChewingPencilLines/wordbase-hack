83fd55c3a8b253640656847ebd508b295794aff1
Jenna Tay Xiu Li <h.p.xiuli@gmail.com>
2017-02-08 14:45:41 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,19 +9,19 @@ angular.module('wordBaseHack', [])
 
     // SERIALISTS missing
     that.puzzle = [
-         'ABCDEFGHIJ',
-         'ABCDEFGHIJ',
-         'ABCDEFGHIJ',
-         'ABCDEFGHIJ',
-         'ABCDEFGHIJ',
-         'ABCDEFGHIJ',
-         'ABCDEFGHIJ',
-         'ABCDEFGHIJ',
-         'ABCDEFGHIJ',
-         'ABCDEFGHIJ',
-         'ABCDEFGHIJ',
-         'ABCDEFGHIJ',
-         'ABCDEFGHIJ',
+         'SMELAZIOYE',
+         'OARTZMRNPA',
+         'UYSESPDOHO',
+         'SEXDAHOEIX',
+         'ISONLIKMRS',
+         'RNFUTOCYAP',
+         'IEGTRPSGIN',
+         'MLSEUAEUJE',
+         'AOVICTIRHD',
+         'SEDASYSYEP',
+         'NSHUROTAME',
+         'DOMALESNLS',
+         'NIPSHINEIT',
       ];
 
     that.startFromBottom = false;
